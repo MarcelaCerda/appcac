d7b1799992125ae4670a64cccbcb87825183993e
marcela <marcela.ines.cerda@gmail.com>
2022-07-20 16:33:49 -0300

    commit new

--- app.py
+++ app.py
@@ -159,9 +159,7 @@ def update_usuario(id):
     return usuario_schema.jsonify(usuario)
 
 
- 
 # programa principal ****************************************
 if __name__=='__main__':  
-    app.run()
+    app.run( debug=True, port=5000)
 
-#debug=True, port=5000
