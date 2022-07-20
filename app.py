1e574759d5aaca0b01870e1e0ee766253f681806
MarcelaCerda <52511815+MarcelaCerda@users.noreply.github.com>
2022-07-20 03:54:58 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -162,5 +162,6 @@ def update_usuario(id):
  
 # programa principal ****************************************
 if __name__=='__main__':  
-    app.run(debug=True, port=5000)
+    app.run()
 
+#debug=True, port=5000
